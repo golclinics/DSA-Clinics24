2840d1bc32fdc9acd11db0a0f2f04e540426ed86
franfreezy <frandelwanjawa@yahoo.com>
2024-09-07 23:11:00 +0300

    Power of two checker v.2

--- franfreezy/power_of_two.py
+++ franfreezy/power_of_two.py
@@ -1,44 +1,27 @@
-class Node:
-    def __init__(self, number):
-        self.number = number
-        self.next=None
-    
+
 
 class Power:
-    def __init__(self):
-        self.head = None
+    def __init__(self,Number):
+        self.Number = Number
        
         
-    def powerchecker(self, number):
+    def powerchecker(self):
         
-        if isinstance(number, int):
-            new_node = Node(number)
+        if self.Number == 1:
+            return True
+                
+        while self.Number % 2 == 0:
+            self.Number = self.Number / 2
             
-            self.head = new_node
-            print(self.head.number)
-            if self.head.number == 1:
-                print('a power of 2')
-                return
+            if self.Number == 1.0:
+                return True
+            elif self.Number % 2 != 0:
+                return False
             
-            current = self.head
-            while (current.number / 2) != 1:
-                
-                
-                if isinstance(current.number, int)==True or float(current.number).is_integer():
-                    current.number = current.number / 2
-                
-                    if current.number == 2:
-                        
-                        print('a power of 2')
-                
-                else:
-                    print('not a power of 2')
-                    break
-                
+            
 
             
-        else:
-            return "not an integer"
+            
 if __name__ == "__main__":
-    p = Power()
-    p.powerchecker(8)
+    p = Power(18)
+    print(p.powerchecker())
