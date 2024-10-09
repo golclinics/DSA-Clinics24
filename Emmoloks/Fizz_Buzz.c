adb25515da13b1a47f5054de6a9f0024355d82f7
Emmoloks <emmanuelkaloki77@gmail.com>
2024-10-09 22:34:22 +0300

    This C program  prints the numbers from 1 to 100 But for multiples of three print Fizz instead of the number and for the multiples of five prints Buzz. For numbers which are multiples of both three and five prints FizzBuzz

---
+++ Emmoloks/Fizz_Buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: FizzBuzz challenge2 in DSAclinics
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
