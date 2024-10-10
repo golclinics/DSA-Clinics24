4c59ea75e57648515d5d6a6043c0b167afaef657
mwangiii <mwangi8kevin@gmail.com>
2024-10-10 13:48:14 +0300

    Issue two:Fizzbuzz implementation

---
+++ mwangiii/fizz_buzz.js
@@ -0,0 +1,16 @@
+let i = 0;
+while(i < 100){
+  i++
+  if(i % 3 == 0 && i % 5 == 0){
+    console.log("FizzBuzz")
+  }
+  else if(i % 3 == 0){
+    console.log("Fizz")
+  }
+  else if(i % 5 == 0){
+    console.log("Buzz")
+  }
+  else{
+    console.log(i)
+  }
+} 
