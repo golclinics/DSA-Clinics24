c40d6a034a86365f54fd2514c1b49a3ae2f90c70
R-Owino <sayeakinyi@gmail.com>
2024-10-10 20:21:46 +0300

    update: add the driver code for 'power_of_two' solution

--- r-owino/dsa-clinic/src/entrance-challenge/src/main.rs
+++ r-owino/dsa-clinic/src/entrance-challenge/src/main.rs
@@ -1,5 +1,6 @@
 mod fizzbuzz;
 mod reverse_integer;
+mod power_of_two;
 
 fn main() {
     println!("FizzBuzz solution:");
@@ -10,4 +11,9 @@ fn main() {
     println!("Reverse integer solution:");
     let x = 500;
     println!("{}", reverse_integer::Solution::reverse_integer(x));
+
+    println!(" ");
+    println!("Power of two solution:");
+    let n = 32;
+    println!("{}", power_of_two::Solution::is_power_of_two(n));
 }
