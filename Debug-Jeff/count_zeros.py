93e1f78075fa2f2091df7775e36a37d27faeb372
@Jeff <Jmutugi2005@gmail.com>
2024-10-10 20:15:11 +0300

    Initial Commit

---
+++ Debug-Jeff/count_zeros.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Run FizzBuzz for numbers 1 to 100
+fizzbuzz(100)
