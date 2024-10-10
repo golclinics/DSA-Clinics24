21e4d8d379ce529d076ee1945efb3af9bc3fcd83
mwangiii <mwangi8kevin@gmail.com>
2024-10-10 13:51:07 +0300

    Issue two:Fizzbuzz implementation

--- mwangiii/fizz_buzz.js
+++ mwangiii/fizz_buzz.js
@@ -1,3 +1,11 @@
+/**
+Entry Challenge C2
+FizzBuzz
+Write a program that prints the numbers from 1 to 100. For multiples of 3, print Fizz; for
+multiples of 5, print Buzz; and for numbers that are multiples of both 3 and 5, print FizzBuzz.
+*/
+
+
 let i = 0;
 while(i < 100){
   i++
