65eaa2b28f9b7f904c4dab0c81842458287ff325
John Karanja <mikaranja@gmail.com>
2024-10-10 14:11:15 +0300

    update

--- mikaranja/reverse_integer.py
+++ mikaranja/reverse_integer.py
@@ -1,13 +1,12 @@
 def reverse_number(x):
-    # Reverse the digits of the number
-    reversed_num = int(str(x)[::-1])
+    if x < 0:
+        reversed_num = -int(str(-x)[::-1])
+    else:
+        reversed_num = int(str(x)[::-1])
     return reversed_num
 
-# Input from the user
 x = int(input("Enter the number: "))
 
-# Get the reversed number
 rev = reverse_number(x)
 
-# Output the reversed number
 print("Reversed number:", rev)
