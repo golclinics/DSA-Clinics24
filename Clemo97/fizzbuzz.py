e96b429fda532c9a3172549ee711ce118667c264
Clemo97 <lumumbaclement@gmai.com>
2024-10-10 09:32:52 +0300

    completed all challenges

---
+++ Clemo97/fizzbuzz.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+fizzbuzz()
