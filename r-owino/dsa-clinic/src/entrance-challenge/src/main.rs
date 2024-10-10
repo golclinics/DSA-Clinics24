5b290176036f695691b1a9fdcfa7d8671e82a333
R-Owino <sayeakinyi@gmail.com>
2024-10-10 20:00:18 +0300

    update: add reverse integer driver code

--- r-owino/dsa-clinic/src/entrance-challenge/src/main.rs
+++ r-owino/dsa-clinic/src/entrance-challenge/src/main.rs
@@ -1,5 +1,13 @@
 mod fizzbuzz;
+mod reverse_integer;
 
 fn main() {
+    println!("FizzBuzz solution:");
     println!("{:?}", fizzbuzz::Solution::fizzbuzz());
+
+    println!(" ");
+
+    println!("Reverse integer solution:");
+    let x = 500;
+    println!("{}", reverse_integer::Solution::reverse_integer(x));
 }
