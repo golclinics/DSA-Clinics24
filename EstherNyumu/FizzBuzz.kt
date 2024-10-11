1b183b55ab046793dbd8a3218cad333329f65e08
EstherNyumu <emutheu1998@gmail.com>
2024-10-11 19:47:09 +0300

    Added solution for Entry Challenge C2 (FizzBuzz)

---
+++ EstherNyumu/FizzBuzz.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    for (i in 1..100) {
+        if((i%3 == 0) && (i%5 == 0)){
+            println("FizzBuzz")
+        }
+        else if(i%3 == 0){
+            println("Buzz")
+        }
+        else if(i%5 == 0){
+            println("Fizz")
+        }
+    }
+}
