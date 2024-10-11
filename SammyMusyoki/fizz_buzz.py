09e4972eab10b2f534739daaaad0374da6e2abc5
SammyMusyoki <sammerkhan328@gmail.com>
2024-10-11 15:34:05 +0300

    [Fix]: Bug-fix

--- SammyMusyoki/fizz_buzz.py
+++ SammyMusyoki/fizz_buzz.py
@@ -7,12 +7,12 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0:
-            print('Fizz')
+        if i % 5 == 0 and i % 3 == 0:
+            print('FizzBuzz')
         elif i % 5 == 0:
             print('Buzz')
-        elif i % 5 == 0 and i % 3 == 0:
-            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
         else:
             print(i)
             
