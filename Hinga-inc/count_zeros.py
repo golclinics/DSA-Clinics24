960e8ebc984a706cb5b49d233f982d4007d177fe
Hinga Peter <hingapeter18@gmail.com>
2024-10-13 17:37:08 +0300

    fizz buzz DSA clinics24 entry challenge

--- Hinga-inc/count_zeros.py
+++ Hinga-inc/count_zeros.py
@@ -1,3 +1,8 @@
+"""
+Write a program that prints the numbers from 1 to 100. For multiples of 3, print Fizz; for
+multiples of 5, print Buzz; and for numbers that are multiples of both 3 and 5, print FizzBuzz.
+"""
+
 for i in range (1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
