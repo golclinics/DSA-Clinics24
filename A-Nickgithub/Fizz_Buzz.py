4381aed18068be4c2c0ac4d8383d5ce1e94861ba
nicholusalbert <nicholusalbertmirindo@gmail.com>
2024-10-13 16:00:32 +0300

    FIZZBUZZ

---
+++ A-Nickgithub/Fizz_Buzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Loop through numbers from 1 to 100
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
