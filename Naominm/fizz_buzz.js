2877eab132e65f17f1b0049ddf06bf3cd233e599
Naomi Mbugua <naomimbugua349@gmail.com>
2024-10-15 08:10:59 +0300

    Entry Challenge C2

--- Naominm/fizz_buzz.js
+++ Naominm/fizz_buzz.js
@@ -1,20 +1,12 @@
-
-
-function fizzBuzz(num) {
-  if (isNaN(num)) {
-    return "Please enter a number";
-  } else if (num % 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } else if (num % 3 === 0) {
-    return "Fizz";
-  } else if (num % 5 === 0) {
-    return "Buzz";
-  } else {    
-    return num;
+for (let num = 1; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+      console.log("Fizz");
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(num);
+    }
   }
-}
-console.log(fizzBuzz(15));
-console.log(fizzBuzz(25));
-console.log(fizzBuzz(9));
-console.log(fizzBuzz(7));
-console.log(fizzBuzz("hello"));
+  
