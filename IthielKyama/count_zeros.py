5a2652767088460391ef25c4cd0bd79c0ef7a962
IthielKyama <nyagaithiel@gmai.com>
2024-10-15 12:26:35 +0300

    take user input

--- IthielKyama/count_zeros.py
+++ IthielKyama/count_zeros.py
@@ -1,12 +1,13 @@
 # Program that takes an integer as input and returns true if the input is a power of two using recursion
 
-def function(n):
-    if n <= 0:
+def function(num):
+    if num <= 0:
         return False
-    elif n == 1:
+    elif num == 1:
         return True
     else:
-        return n % 2 == 0 and function(n // 2)
+        return num % 2 == 0 and function(num // 2)
 
 
-print(function(4))
+n = int(input("Input integer: "))
+print(function(n))
