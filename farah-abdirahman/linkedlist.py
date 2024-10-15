f92b0d96e721c8ad1ef5ebbf8131d66eb5833465
bigboyfreezy <abdifarah076@gmail.com>
2024-10-15 19:59:50 +0300

    Update on the code

--- farah-abdirahman/linkedlist.py
+++ farah-abdirahman/linkedlist.py
@@ -49,11 +49,7 @@ my_linked_list = LinkedList(1)
 my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
+my_linked_list.append(5)
 
-print('LL before reverse():')
-my_linked_list.print_list()
-
-my_linked_list.reverse()
-
-print('\nLL after reverse():')
+print('\Reversed Linked List:')
 my_linked_list.print_list()
