8069f32d0946c389aeade759b992f9643d0352be
Thiak Ayuen <129801519+ThiakMaleth@users.noreply.github.com>
2024-10-17 13:20:56 +0000

    some improvements

--- Thiak -Maleth/Multiples.java
+++ Thiak -Maleth/Multiples.java
@@ -6,8 +6,8 @@ and for numbers that are multiples of both 3 and 5, print FizzBuzz.
 
 public class Multiples {
 
-    public static void multiples(int number) {
-        for (int i = 0; i <= number; i++){
+    public static void multiples() {
+        for (int i = 1; i <= 100; i++){
             if (i%3 == 0 && i%5 == 0) {
                 System.out.println("FizzBuzz");
             }
@@ -15,18 +15,13 @@ public class Multiples {
                 System.out.println("Fizz");
             } else if (i%5 == 0){
                 System.out.println("Fuzz");
-           } 
+           } else {
+            System.out.println(i);
+           }
         }
     }
 
     public static void main(String[] args) {
-        int number = 100;
-        multiples(number);
-    }
-}
-
-    public static void main(String[] args) {
-        int number = 100;
-        multiples(number);
+        multiples();
     }
 }
