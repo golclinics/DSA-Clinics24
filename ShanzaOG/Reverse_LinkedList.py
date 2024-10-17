9d29b2ef1c557e3cdc03252b80b176769cef4958
Shanza Allan <ken22958@kcbgroup.com>
2024-10-17 19:25:41 +0300

    Reversing singly linked lists

---
+++ ShanzaOG/Reverse_LinkedList.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head):
+        prev = None  # Set Previous
+        current = head
+
+        while current:
+            nxt = current.next
+            current.next = prev
+            prev = current
+            current = nxt
+        return prev
